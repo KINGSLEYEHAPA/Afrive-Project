2156c9985cb3fdba97bfeb269b53af8e995c4b7b
KINGSLEYEHAPA <kessity09@gmail.com>
2022-05-28 07:27:31 +0100

    Completed the recommended boooks section

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./Header";
 
 import HeroSection from "./HeroSection";
+import RecommendedBooks from "./RecommendedBooks";
 
 const Homepage = () => {
   return (
     <div className="w-screen max-w-[1440px] mx-auto mt-[88px] ">
       <Header />
       <HeroSection />
+      <RecommendedBooks />
     </div>
   );
 };
