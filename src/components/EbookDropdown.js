8803cca09c19e0d44458b6cd8debf8a75e085db7
KINGSLEYEHAPA <kessity09@gmail.com>
2022-05-29 06:25:31 +0100

    favorite functionality on book implemented

--- src/components/EbookDropdown.js
+++ src/components/EbookDropdown.js
@@ -9,112 +9,134 @@ const EbookDropdown = () => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1, transition: { duration: 1 } }}
       exit={{ opacity: 0, transition: { duration: 1 } }}
-      className=" absolute top-[100px] z-10 right-[-388px] w-[494px] h-[410px] bg-neutral-white border-[0.5px solid rgba(0,0,0,0.1)] shadow-[0px 4px 8px rgba(0, 0, 0, 0.15)] rounded-[4px] px-[32px] pt-[29px] pb-[26.55px]  overflow-hidden overflow-y-auto scrollbar-hide"
+      className=" absolute top-[100px] z-10 right-[-388px] w-[494px] h-[410px] bg-neutral-white border-[0.5px solid rgba(0,0,0,0.1)] shadow-[0px 4px 8px rgba(0, 0, 0, 0.15)] rounded-[4px] px-[32px]   "
     >
-      <div className="cursor-pointer">
-        <div className="w-[356px] h-[86.45px] flex items-center justify-start gap-[40px]">
-          <img className="w-[63px] h-[86.45px]" src={bookSmall} alt="E-Book" />
-          <div className=" h-[86.45px] mt-[28px]">
-            {" "}
-            <h3 className="whitespace-nowrap font-medium text-[black] text-bodyL">
-              How to break a Sun
-              <span className="ml-[20px] text-[rgba(0, 0, 0, 0.2)] text-bodyS font-reg">
-                PDF
-              </span>
-            </h3>{" "}
-            <p className="whitespace-nowrap mt-[8px] font-reg text-[black] text-bodyS">
-              Chimamanda Adichie
+      <div className="mt-[29px] mb-[26.55px] h-[354.45px]  overflow-hidden overflow-y-auto scrollbar-hide ">
+        <div className="cursor-pointer ">
+          <div className="w-[356px] h-[86.45px] flex items-center justify-start gap-[40px]">
+            <img
+              className="w-[63px] h-[86.45px]"
+              src={bookSmall}
+              alt="E-Book"
+            />
+            <div className=" h-[86.45px] mt-[28px]">
+              {" "}
+              <h3 className="whitespace-nowrap font-medium text-[black] text-bodyL">
+                How to break a Sun
+                <span className="ml-[20px] text-[rgba(0, 0, 0, 0.2)] text-bodyS font-reg">
+                  PDF
+                </span>
+              </h3>{" "}
+              <p className="whitespace-nowrap mt-[8px] font-reg text-[black] text-bodyS">
+                Chimamanda Adichie
+              </p>
+            </div>
+            <p className="w-[74px] h-[18px] font-red text-primary-30 text-bodyS">
+              26/05/2022
             </p>
           </div>
-          <p className="w-[74px] h-[18px] font-red text-primary-30 text-bodyS">
-            26/05/2022
-          </p>
+          <hr className="w-[80%] mx-auto  h-0 border-1 border-neutral-20 mt-[23.55px] mb-[23px]" />
         </div>
-        <hr className="w-[80%] mx-auto  h-0 border-1 border-neutral-20 mt-[23.55px] mb-[23px]" />
-      </div>
-      <div>
-        <div className="w-[356px] h-[86.45px] flex items-center justify-start gap-[40px]">
-          <img className="w-[63px] h-[86.45px]" src={bookSmall} alt="E-Book" />
-          <div className=" h-[86.45px] mt-[28px]">
-            {" "}
-            <h3 className="whitespace-nowrap font-medium text-[black] text-bodyL">
-              How to break a Sun
-              <span className="ml-[20px] text-[rgba(0, 0, 0, 0.2)] text-bodyS font-reg">
-                PDF
-              </span>
-            </h3>{" "}
-            <p className="whitespace-nowrap mt-[8px] font-reg text-[black] text-bodyS">
-              Chimamanda Adichie
+        <div>
+          <div className="w-[356px] h-[86.45px] flex items-center justify-start gap-[40px]">
+            <img
+              className="w-[63px] h-[86.45px]"
+              src={bookSmall}
+              alt="E-Book"
+            />
+            <div className=" h-[86.45px] mt-[28px]">
+              {" "}
+              <h3 className="whitespace-nowrap font-medium text-[black] text-bodyL">
+                How to break a Sun
+                <span className="ml-[20px] text-[rgba(0, 0, 0, 0.2)] text-bodyS font-reg">
+                  PDF
+                </span>
+              </h3>{" "}
+              <p className="whitespace-nowrap mt-[8px] font-reg text-[black] text-bodyS">
+                Chimamanda Adichie
+              </p>
+            </div>
+            <p className="w-[74px] h-[18px] font-red text-primary-30 text-bodyS">
+              26/05/2022
             </p>
           </div>
-          <p className="w-[74px] h-[18px] font-red text-primary-30 text-bodyS">
-            26/05/2022
-          </p>
+          <hr className="w-[80%] mx-auto  h-0 border-1 border-neutral-20 mt-[23.55px]  mb-[23px]" />
         </div>
-        <hr className="w-[80%] mx-auto  h-0 border-1 border-neutral-20 mt-[23.55px]  mb-[23px]" />
-      </div>
-      <div>
-        <div className="w-[356px] h-[86.45px] flex items-center justify-start gap-[40px]">
-          <img className="w-[63px] h-[86.45px]" src={bookSmall} alt="E-Book" />
-          <div className=" h-[86.45px] mt-[28px]">
-            {" "}
-            <h3 className="whitespace-nowrap font-medium text-[black] text-bodyL">
-              How to break a Sun
-              <span className="ml-[20px] text-[rgba(0, 0, 0, 0.2)] text-bodyS font-reg">
-                PDF
-              </span>
-            </h3>{" "}
-            <p className="whitespace-nowrap mt-[8px] font-reg text-[black] text-bodyS">
-              Chimamanda Adichie
+        <div>
+          <div className="w-[356px] h-[86.45px] flex items-center justify-start gap-[40px]">
+            <img
+              className="w-[63px] h-[86.45px]"
+              src={bookSmall}
+              alt="E-Book"
+            />
+            <div className=" h-[86.45px] mt-[28px]">
+              {" "}
+              <h3 className="whitespace-nowrap font-medium text-[black] text-bodyL">
+                How to break a Sun
+                <span className="ml-[20px] text-[rgba(0, 0, 0, 0.2)] text-bodyS font-reg">
+                  PDF
+                </span>
+              </h3>{" "}
+              <p className="whitespace-nowrap mt-[8px] font-reg text-[black] text-bodyS">
+                Chimamanda Adichie
+              </p>
+            </div>
+            <p className="w-[74px] h-[18px] font-red text-primary-30 text-bodyS">
+              26/05/2022
             </p>
           </div>
-          <p className="w-[74px] h-[18px] font-red text-primary-30 text-bodyS">
-            26/05/2022
-          </p>
+          <hr className="w-[80%] mx-auto  h-0 border-1 border-neutral-20 mt-[23.55px]  mb-[23px]" />
         </div>
-        <hr className="w-[80%] mx-auto  h-0 border-1 border-neutral-20 mt-[23.55px]  mb-[23px]" />
-      </div>
-      <div>
-        <div className="w-[356px] h-[86.45px] flex items-center justify-start gap-[40px]">
-          <img className="w-[63px] h-[86.45px]" src={bookSmall} alt="E-Book" />
-          <div className=" h-[86.45px] mt-[28px]">
-            {" "}
-            <h3 className="whitespace-nowrap font-medium text-[black] text-bodyL">
-              How to break a Sun
-              <span className="ml-[20px] text-[rgba(0, 0, 0, 0.2)] text-bodyS font-reg">
-                PDF
-              </span>
-            </h3>{" "}
-            <p className="whitespace-nowrap mt-[8px] font-reg text-[black] text-bodyS">
-              Chimamanda Adichie
+        <div>
+          <div className="w-[356px] h-[86.45px] flex items-center justify-start gap-[40px]">
+            <img
+              className="w-[63px] h-[86.45px]"
+              src={bookSmall}
+              alt="E-Book"
+            />
+            <div className=" h-[86.45px] mt-[28px]">
+              {" "}
+              <h3 className="whitespace-nowrap font-medium text-[black] text-bodyL">
+                How to break a Sun
+                <span className="ml-[20px] text-[rgba(0, 0, 0, 0.2)] text-bodyS font-reg">
+                  PDF
+                </span>
+              </h3>{" "}
+              <p className="whitespace-nowrap mt-[8px] font-reg text-[black] text-bodyS">
+                Chimamanda Adichie
+              </p>
+            </div>
+            <p className="w-[74px] h-[18px] font-red text-primary-30 text-bodyS">
+              26/05/2022
             </p>
           </div>
-          <p className="w-[74px] h-[18px] font-red text-primary-30 text-bodyS">
-            26/05/2022
-          </p>
+          <hr className="w-[80%] mx-auto  h-0 border-1 border-neutral-20 mt-[23.55px]  mb-[23px]" />
         </div>
-        <hr className="w-[80%] mx-auto  h-0 border-1 border-neutral-20 mt-[23.55px]  mb-[23px]" />
-      </div>
-      <div>
-        <div className="w-[356px] h-[86.45px] flex items-center justify-start gap-[40px]">
-          <img className="w-[63px] h-[86.45px]" src={bookSmall} alt="E-Book" />
-          <div className=" h-[86.45px] mt-[28px]">
-            {" "}
-            <h3 className="whitespace-nowrap font-medium text-[black] text-bodyL">
-              How to break a Sun
-              <span className="ml-[20px] text-[rgba(0, 0, 0, 0.2)] text-bodyS font-reg">
-                PDF
-              </span>
-            </h3>{" "}
-            <p className="whitespace-nowrap mt-[8px] font-reg text-[black] text-bodyS">
-              Chimamanda Adichie
+        <div>
+          <div className="w-[356px] h-[86.45px] flex items-center justify-start gap-[40px]">
+            <img
+              className="w-[63px] h-[86.45px]"
+              src={bookSmall}
+              alt="E-Book"
+            />
+            <div className=" h-[86.45px] mt-[28px]">
+              {" "}
+              <h3 className="whitespace-nowrap font-medium text-[black] text-bodyL">
+                How to break a Sun
+                <span className="ml-[20px] text-[rgba(0, 0, 0, 0.2)] text-bodyS font-reg">
+                  PDF
+                </span>
+              </h3>{" "}
+              <p className="whitespace-nowrap mt-[8px] font-reg text-[black] text-bodyS">
+                Chimamanda Adichie
+              </p>
+            </div>
+            <p className="w-[74px] h-[18px] font-red text-primary-30 text-bodyS">
+              26/05/2022
             </p>
           </div>
-          <p className="w-[74px] h-[18px] font-red text-primary-30 text-bodyS">
-            26/05/2022
-          </p>
+          <hr className="w-[80%] mx-auto  h-0 border-1 border-neutral-20 mt-[23.55px]  mb-[23px]" />
         </div>
-        <hr className="w-[80%] mx-auto  h-0 border-1 border-neutral-20 mt-[23.55px]  mb-[23px]" />
       </div>
     </motion.div>
   );
