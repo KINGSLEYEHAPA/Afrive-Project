ff2841a844366757b2ab3d1488710d483cde490c
KINGSLEYEHAPA <kessity09@gmail.com>
2022-07-04 07:45:11 +0100

    Updating pagination component

--- src/components/BookPagination.js
+++ src/components/BookPagination.js
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+import { Link as ALink } from "react-scroll";
 
 const BookPagination = ({
   totalBooks,
@@ -15,14 +16,22 @@ const BookPagination = ({
     pageNumbers.push(i);
   }
   return (
-    <nav className="w-full flex justify-center items-center mt-[86.51px]">
-      <span
-        onClick={() => currentPage > 1 && paginate(currentPage - 1)}
-        className="text-neutral-black mx-[39px] text-[20px] cursor-pointer"
+    <nav className="w-full flex gap-[24px] justify-center items-center mt-[86.51px]">
+      <ALink
+        to="category-title"
+        spy={true}
+        smooth={true}
+        offset={-120}
+        duration={1000}
       >
-        <MdChevronLeft />
-      </span>
-      <ul className="flex gap-[24px]">
+        <span
+          onClick={() => currentPage > 1 && paginate(currentPage - 1)}
+          className="text-neutral-black mx-[39px] text-[25px] cursor-pointer"
+        >
+          <MdChevronLeft />
+        </span>
+      </ALink>
+      <ul className="flex justify-center relative right-[8px] items-center gap-[24px]">
         {pageNumbers.map((number) => (
           <li
             key={number}
@@ -32,25 +41,41 @@ const BookPagination = ({
                 : "text-bodyL text-primary-40 "
             }
           >
-            <NavLink
-              to={
-                paginationtype === "all"
-                  ? `/all-books/${catName}`
-                  : `/category/${catName}`
-              }
-              onClick={() => paginate(number)}
+            <ALink
+              to="category-title"
+              spy={true}
+              smooth={true}
+              offset={-120}
+              duration={1000}
             >
-              {number}
-            </NavLink>
+              <NavLink
+                to={
+                  paginationtype === "all"
+                    ? `/all-books/${catName}`
+                    : `/category/${catName}`
+                }
+                onClick={() => paginate(number)}
+              >
+                {number}
+              </NavLink>
+            </ALink>
           </li>
         ))}
       </ul>
-      <span
-        onClick={() => currentPage < totalPages && paginate(currentPage + 1)}
-        className="text-neutral-black mx-[39px] text-[20px] cursor-pointer"
+      <ALink
+        to="category-title"
+        spy={true}
+        smooth={true}
+        offset={-120}
+        duration={1000}
       >
-        <MdChevronRight />
-      </span>
+        <span
+          onClick={() => currentPage < totalPages && paginate(currentPage + 1)}
+          className="text-neutral-black mx-[39px] text-[25px] cursor-pointer"
+        >
+          <MdChevronRight />
+        </span>
+      </ALink>
     </nav>
   );
 };
