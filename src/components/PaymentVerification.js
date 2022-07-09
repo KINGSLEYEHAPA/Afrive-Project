797489516cba3b50da9005f136547dff9f4bd76f
KINGSLEYEHAPA <kessity09@gmail.com>
2022-07-09 23:28:28 +0100

    Update on Payment Logic

--- src/components/PaymentVerification.js
+++ src/components/PaymentVerification.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { BsBagCheckFill } from "react-icons/bs";
 import { useDispatch, useSelector } from "react-redux";
+import { motion, AnimatePresence } from "framer-motion";
 import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
 import {
   clearPaymentState,
@@ -37,48 +38,60 @@ const PaymentVerification = () => {
   console.log(location.search);
   return (
     <div className="w-[505px]  h-[510px] bg-neutral-white">
-      {payState === 0 && (
-        <div className="w-full h-full flex items-center justify-center flex-col">
-          <h1 className="text-h2 text-primary-50">Afrive Bookstore </h1>
-          <h2 className="text-neutral-40 text-bodyL mt-[25px]">
-            {" "}
-            Payment initialised.....Verifying payment
-          </h2>
-          <div className="mt-[-200px]">
-            {" "}
-            <Loading />
-          </div>
-        </div>
-      )}
-      {payState === 1 && (
-        <div className="bg-neutral-white p-[22px]">
-          <div className="h-[380px] w-full flex flex-col justify-center items-center gap-[15px]">
-            <h3 className="text-primary-50 text-h3">Afrive Books WebStore</h3>
-            <span className="text-[60px] text-primary-50">
-              <BsBagCheckFill />
-            </span>
-            <h2 className="text-h4 font-medium text-primary-50">
-              Payment Verified
-            </h2>
-            <p className="text-bodyN text-neutral-40 leading-7">
-              Your order has been processed and your transaction reference is{" "}
-              <span className="text-primary-60">
-                {" "}
-                {paymentVerified?.data?.txn_ref}
-              </span>
-            </p>
-            <p className="text-primary-60 text-bodyL">
-              Thank you for your purchase!
-            </p>
-          </div>
-          <button
-            onClick={() => navigate("/")}
-            className="w-full h-[65px] mt-[20px] rounded-[4px] bg-primary-50 text-neutral-white text-bodyL font-medium"
+      <AnimatePresence>
+        {payState === 0 && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1, transition: { duration: 1.2 } }}
+            exit={{ opacity: 0, transition: { duration: 1.2 } }}
+            className="w-full h-full flex items-center justify-center flex-col"
           >
-            Return to Home
-          </button>
-        </div>
-      )}
+            <h1 className="text-h2 text-primary-50">Afrive Bookstore </h1>
+            <h2 className="text-neutral-40 text-bodyL mt-[25px]">
+              {" "}
+              Payment initialised.....Verifying payment
+            </h2>
+            <div className="mt-[-200px]">
+              {" "}
+              <Loading />
+            </div>
+          </motion.div>
+        )}
+        {payState === 1 && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1, transition: { duration: 1.2 } }}
+            exit={{ opacity: 0, transition: { duration: 1.2 } }}
+            className="bg-neutral-white p-[22px]"
+          >
+            <div className="h-[380px] w-full flex flex-col justify-center items-center gap-[15px]">
+              <h3 className="text-primary-50 text-h3">Afrive Books WebStore</h3>
+              <span className="text-[60px] text-primary-50">
+                <BsBagCheckFill />
+              </span>
+              <h2 className="text-h4 font-medium text-primary-50">
+                Payment Verified
+              </h2>
+              <p className="text-bodyN text-neutral-40 leading-7">
+                Your order has been processed and your transaction reference is{" "}
+                <span className="text-primary-60">
+                  {" "}
+                  {paymentVerified?.data?.txn_ref}
+                </span>
+              </p>
+              <p className="text-primary-60 text-bodyL">
+                Thank you for your purchase!
+              </p>
+            </div>
+            <button
+              onClick={() => navigate("/")}
+              className="w-full h-[65px] mt-[20px] rounded-[4px] bg-primary-50 text-neutral-white text-bodyL font-medium"
+            >
+              Return to Home
+            </button>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
