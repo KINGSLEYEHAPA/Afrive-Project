e029df2122dfeacd28ea01f2a135f6e8a7cd166b
KINGSLEYEHAPA <kessity09@gmail.com>
2022-07-11 22:51:42 +0100

    Working on responsiveness for Book preview page

--- src/components/BookPreview.js
+++ src/components/BookPreview.js
@@ -493,19 +493,20 @@ const BookPreview = () => {
         <CustomerBookReview book={selectedBook} />
         <BookSection bookSectionName="Similar Books" />
         <BookSection bookSectionName="Best Sellers" />
-        <div className="w-full mt-[72.51px] flex justify-end items-center pr-[117.23px] mb-[-19px]">
+        <div className="w-full mt-[72.51px] flex justify-end items-center pr-[24px] mtab:pr-[71.14px] lap:pr-[117.23px] mb-[-19px]">
           <div className="  h-[96px] flex justify-start items-center gap-[5px]  ">
-            <span className="text-[25px]">
+            <span className="text-[15px]   lap:text-[25px]">
               <FiArrowUpLeft />
             </span>
+
             <ALink
-              to="preview-title"
+              to="liked-books"
               spy={true}
               smooth={true}
               offset={-120}
               duration={1000}
             >
-              <p className="text-h4 font-reg text-[#000000] cursor-pointer active:text-primary-50">
+              <p className="text-sub mtab:text-bodyS   lap:text-h4 font-reg text-[#000000] cursor-pointer active:text-primary-50">
                 Back to Top
               </p>
             </ALink>
