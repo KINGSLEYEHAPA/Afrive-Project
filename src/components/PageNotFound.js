a4ebff15665fb0f8c6a4edcd80abb145670b908f
KINGSLEYEHAPA <kessity09@gmail.com>
2022-07-16 09:08:17 +0100

    worked on optimizing page not found page

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -5,15 +5,17 @@ const PageNotFound = () => {
   return (
     <div className="w-screen max-w-[1440px]  mx-auto mt-[196px] flex justify-center flex-col items-center gap-[81.09px] ">
       <div className="text-center">
-        <p className="w-[602px] h-[64px] text-h4 font-reg text-neutral-70">
+        <p className=" w-[360px]   moby:w-[602px] h-[64px] text-h4 font-reg text-neutral-70">
           Hey! We got lost loading up your request...but don’t worry we’re
           working to find this page for you.
         </p>
       </div>
-      <div className=" w-[539px] h-[388.49px">
+      <div className=" w-[360px] h-[400px]   moby:w-[539px]   tab:w-[539px]  moby:h-[600px]   tab:h-[388.49px]">
         <img className="w-full h-full" src={lostRoute} alt="Lost Route" />
       </div>
-      <BookQuote />
+      <div className=" mt-[125px]  moby:mt-[400px] tab:mt-0 ">
+        <BookQuote />
+      </div>
     </div>
   );
 };
