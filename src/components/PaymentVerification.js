bf9ec819a35ee90a1bac416c4cd84cf87a3a8a9a
KINGSLEYEHAPA <kessity09@gmail.com>
2022-08-04 22:10:35 +0100

    adjustment to payment component

--- src/components/PaymentVerification.js
+++ src/components/PaymentVerification.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { BsBagCheckFill } from "react-icons/bs";
+import { ImWarning } from "react-icons/im";
 import { useDispatch, useSelector } from "react-redux";
 import { motion, AnimatePresence } from "framer-motion";
 import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
@@ -30,6 +31,11 @@ const PaymentVerification = () => {
     if (!isPaymentLoading && isPaymentSuccessfull && !isError) {
       setTimeout(() => {
         setPayState(1);
+        dispatch(clearUserPreference());
+      }, 3000);
+    } else if (!isPaymentLoading && !isPaymentSuccessfull && isError) {
+      setTimeout(() => {
+        setPayState(2);
         dispatch(clearUserPreference());
       }, 3000);
     }
@@ -83,6 +89,35 @@ const PaymentVerification = () => {
                 <span className="text-primary-60 text-bodyS  moby:text-bodyN ">
                   Thank you for your purchase!
                 </span>
+              </p>
+            </div>
+            <button
+              onClick={() => navigate("/")}
+              className="w-full h-[48px]     moby:h-[65px] mt-[20px] rounded-[4px] bg-primary-50 text-neutral-white text-bodyN   moby:text-bodyL font-medium"
+            >
+              Return to Home
+            </button>
+          </motion.div>
+        )}
+        {payState === 2 && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1, transition: { duration: 1.2 } }}
+            exit={{ opacity: 0, transition: { duration: 1.2 } }}
+            className="  w-full  bg-neutral-white p-[10px]"
+          >
+            <div className=" h-[360px]   moby:h-[380px] w-full flex flex-col justify-center items-center gap-[15px]">
+              <h3 className="text-primary-50  text-bodyL moby:text-h3    whitespace-nowrap">
+                Afrive Books WebStore
+              </h3>
+              <span className=" text-[40px]   moby:text-[60px] text-primary-50">
+                <ImWarning />
+              </span>
+              <h2 className="  text-bodyN  moby:text-h4 font-medium text-primary-50">
+                Payment not Verified
+              </h2>
+              <p className=" text-bodyS  moby:text-bodyN text-neutral-40 leading-7">
+                You can see details of the failed transaction in the orders page
               </p>
             </div>
             <button
