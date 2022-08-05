f6a848abfa41d3e9c1b3be2cf5851992c43cf0a3
KINGSLEYEHAPA <kessity09@gmail.com>
2022-08-05 11:42:37 +0100

    Recommended changes to UI

--- src/components/LikedBooks.js
+++ src/components/LikedBooks.js
@@ -7,9 +7,11 @@ import LikedbookSection from "./LikedbookSection";
 import { FiArrowUpLeft } from "react-icons/fi";
 import { Link as ALink } from "react-scroll";
 import AnimatePages from "./AnimatePages";
+import { useSelector } from "react-redux";
 
 const LikedBooks = () => {
   const navigate = useNavigate();
+  const favoriteBooks = useSelector((state) => state.books.likedBooks);
 
   return (
     <AnimatePages>
@@ -37,6 +39,11 @@ const LikedBooks = () => {
             you can find the books you have liked here
           </span>
         </div>
+        {favoriteBooks.length === 0 && (
+          <div className=" flex justify-center items-center translate-x-[-5%] text-bodyN tab:text-bodyL font-reg text-neutral-30     w-full  h-[80vh] mtab:pl-[72px] tab:pl-[86px] lap:pl-[193px]  desk:pl-[181px] mtab:pr-[24px] tab:pr-[50px]  lap:pr-[40px] desk:pr-[10px]  ">
+            You have no Favorite Book.
+          </div>
+        )}
         <LikedbookSection />
         <div className="w-full mt-[72.51px] flex justify-end items-center pr-[24px] mtab:pr-[71.14px] lap:pr-[117.23px] mb-[-19px]">
           <div className="  h-[96px] flex justify-start items-center gap-[5px]  ">
