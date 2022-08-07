1de4b7ed2b79da4b00d05c0044045bba784ab56c
KINGSLEYEHAPA <kessity09@gmail.com>
2022-08-07 12:28:19 +0100

    Making changes to some UI elements in the application

--- src/components/BookPreview.js
+++ src/components/BookPreview.js
@@ -371,7 +371,7 @@ const BookPreview = () => {
             </div>
             <div className=" hidden mtab:flex mtab:mt-[12.73px]    lap:mt-[24px] w-[391px] h-[32px] justify-start items-center text-neutral-80 font-reg text-bodyN">
               <h3>Category: &nbsp;</h3>
-              <p className="text-primary-30">
+              <p className="text-primary-30 whitespace-nowrap">
                 {selectedBook?.category?.slice(0, 4).map((item) => {
                   return <span key={item}>{item},&nbsp;</span>;
                 })}
@@ -381,7 +381,7 @@ const BookPreview = () => {
         </div>
         <div className=" text-sub px-[24px] mobx:px-[40px] mtab:hidden mt-[24px] w-[391px] h-[32px] flex justify-start items-center text-neutral-80 font-reg ">
           <h3>Category: &nbsp;</h3>
-          <p className="text-primary-30">
+          <p className="text-primary-30 whitespace-nowrap">
             {selectedBook?.category?.slice(0, 4).map((item) => {
               return <span key={item}>{item},&nbsp;</span>;
             })}
